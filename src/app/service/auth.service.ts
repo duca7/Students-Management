de55086852e6110188c7dca2a05903c818a369d2
duc <hohuynhduca7bh123@gmail.com>
2020-06-12 21:23:02 +0700

    auth service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { User } from '../model/user.model';
+import { switchMap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) { }
+
+  user$: Observable<any>;
+
+  checkCurrentUser() {
+    return this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.db.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+
+  async logOut() {
+    await this.afAuth.signOut();
+    return this.router.navigate(['/']);
+  }
+
+  updateUserData({ uid, email, photoURL }: User) {
+    const userRef: AngularFirestoreDocument<User> = this.db.doc(`users/${uid}`);
+    const data = {
+      uid,
+      email,
+      photoURL
+    };
+    return userRef.set(data, { merge: true });
+  }
+
 }
